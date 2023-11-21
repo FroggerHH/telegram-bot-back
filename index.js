552af266bd998514a3abf873c825bab253392068
Vyacheslav <slava.shilin07@mail.ru>
2023-11-21 15:41:35 +0300

    Added bot

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const TelegramBot = require('node-telegram-bot-api');
 
-app.use(express.json());
-app.use(cors());
+const token = '6693653778:AAGjEF3T4kRJut4ebfJsNEKAjS6s2eFpRQQ';
+const webAppUrl = 'https://jf-tg-bot.netlify.app/';
 
-const PORT = 8000;
-app.listen(PORT, () => console.log('server started on PORT ' + PORT))
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ],
+                resize_keyboard: true,
+                one_time_keyboard: true
+            }
+        })
+
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+
+    if (msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+});
