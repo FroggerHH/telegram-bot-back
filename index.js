9640d2163724c1bcf52b3767799e0ed6be07cd41
Vyacheslav <slava.shilin07@mail.ru>
2023-11-21 15:34:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('server started');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8000;
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
