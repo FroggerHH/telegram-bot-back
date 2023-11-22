eeaf3d13dfc609f2e606a09a4d51d24ca349fd26
Vyacheslav <slava.shilin07@mail.ru>
2023-11-22 12:20:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -82,6 +82,26 @@ app.post('api/web-data', async (req, res) => {
     }
 })
 
+app.get('/api/get-products', async (req, res) => {
+    const products = [
+        {
+            id: '1',
+            title: 'Джинсы',
+            price: 5000,
+            description: 'Синего цвета, прямые',
+            picture: 'https://hips.hearstapps.com/hmg-prod/images/jeans-scattered-on-a-wooden-background-royalty-free-image-940015420-1551972989.jpg'
+        },
+        {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+        {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+        {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+        {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+        {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+        {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+        {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    ]
+    res.status(200).json(products)
+})
+
 app.get('/', (req, res) => {
     res.send(`
     <!DOCTYPE html>
