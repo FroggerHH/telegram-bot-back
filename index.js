09a2908f214ad6058f238993e184bb9bdf41f3c3
Vyacheslav <slava.shilin07@mail.ru>
2023-11-26 13:32:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const webAppUrl = 'https://jf-tg-bot.netlify.app/';
 const io = require('@pm2/io')
 io.init({
     http: true, https: true, transactions: true
@@ -10,7 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
 
 app.get('/api/get-products', async (req, res) => {
     const products = [
@@ -57,92 +55,92 @@ app.get('/api/get-products', async (req, res) => {
 app.get('/api', (req, res) => {
     res.send(`
     <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API Documentation</title>
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-        }
-
-        header {
-            text-align: center;
-            padding: 20px;
-            background-color: #f2f2f2;
-        }
-
-        h1 {
-            color: #333;
-        }
-
-        section {
-            margin-top: 20px;
-        }
-
-        p {
-            line-height: 1.6;
-            color: #555;
-        }
-
-        footer {
-            text-align: center;
-            color: #888;
-        }
-
-        html{
-            height: 100%;
-        }
-
-        body{
-            height: 100%;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            flex-direction: column;
-        }
-
-        .code{
-            background-color: darkgray;
-            border-radius: 5px;
-            padding: 1px 4px;
-        }
-    </style>
-</head>
-<body>
-<main>
-    <header>
-        <h1>API Documentation</h1>
-    </header>
-    <section>
-        <h2>Introduction</h2>
-        <p>Welcome to the documentation for our API. This API provides various features for my Telegram bot to work.</p>
-    </section>
-    <section>
-        <h2>Endpoints</h2>
-        <p>Our API has the following endpoints:</p>
-        <ul>
-            <li>/ - GET - Opens this page</li>
-            <li>/api - GET - Redirects to <span class="code">/</span> page.</li>
-            <li>/api/get-user-photo - GET</li>
-            <li>/api/get-products - GET - Returns an array of all products</li>
-            <!-- Add more endpoints and descriptions as needed -->
-        </ul>
-    </section>
-</main>
-<footer>
-    &copy; 2023 My Telegram Bot<br>
-    To access the API, you should talk to author. Contact me in discord
-    <span class="code">@justafrogger</span>
-</footer>
-
-</body>
-</html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>API Documentation</title>
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+            }
+    
+            header {
+                text-align: center;
+                padding: 20px;
+                background-color: #f2f2f2;
+            }
+    
+            h1 {
+                color: #333;
+            }
+    
+            section {
+                margin-top: 20px;
+            }
+    
+            p {
+                line-height: 1.6;
+                color: #555;
+            }
+    
+            footer {
+                text-align: center;
+                color: #888;
+            }
+    
+            html{
+                height: 100%;
+            }
+    
+            body{
+                height: 100%;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                flex-direction: column;
+            }
+    
+            .code{
+                background-color: darkgray;
+                border-radius: 5px;
+                padding: 1px 4px;
+            }
+        </style>
+    </head>
+    <body>
+    <main>
+        <header>
+            <h1>API Documentation</h1>
+        </header>
+        <section>
+            <h2>Introduction</h2>
+            <p>Welcome to the documentation for our API. This API provides various features for my Telegram bot to work.</p>
+        </section>
+        <section>
+            <h2>Endpoints</h2>
+            <p>Our API has the following endpoints:</p>
+            <ul>
+                <li>/ - GET - Opens this page</li>
+                <li>/api - GET - Redirects to <span class="code">/</span> page.</li>
+                <li>/api/get-user-photo - GET</li>
+                <li>/api/get-products - GET - Returns an array of all products</li>
+                <!-- Add more endpoints and descriptions as needed -->
+            </ul>
+        </section>
+    </main>
+    <footer>
+        &copy; 2023 My Telegram Bot<br>
+        To access the API, you should talk to author. Contact me in discord
+        <span class="code">@justafrogger</span>
+    </footer>
+    
+    </body>
+    </html>
 `);
 })
 
